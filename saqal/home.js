38f286cd6d9f1079d213c832d2b654ad9093d38c
Bassam A <mutairibassam@gmail.com>
2024-12-14 21:32:47 +0300

    feat: redesign student challenge to start with designing a simple login api as basic authentication.

---
+++ saqal/home.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use((req, res, next) => {
+    if(req.url !== '/') {
+        return res.status(404).sendFile(path.join(__dirname, '404.html'));
+    }
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- shorten-url/README.md
+++
@@ -1,3 +0,0 @@
-In this challenge student needs to design & develop a shorten url application that helps employees to shorten urls.
-
-Application should be accessible by all employees within the network.
