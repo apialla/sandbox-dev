82f5dad84be0fa5e2515f38f5a1dced228e2e21d
Bassam A <mutairibassam@gmail.com>
2024-12-20 09:40:20 +0300

    feat: add saqal container

--- saqal/home.js
+++ saqal/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 65111
 
 app.use((req, res, next) => {
     if(req.url !== '/') {
